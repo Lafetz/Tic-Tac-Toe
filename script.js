b4263beb8bedb50cd85eac388ccfc48d703f86e0
cosmic <yafetyohaness@gmail.com>
2022-05-02 00:56:14 +0300

    Add function to check game status

--- script.js
+++ script.js
@@ -3,6 +3,16 @@
 // 6 7 8
 let gameBoard = (function () {
   let grids = ["", "", "", "", "", "", "", "", ""];
+  const gameGrid = [
+    [0, 1, 2], //1
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], //4
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8], //7
+    [2, 4, 6],
+  ];
   let firstChoice = true; //check who started //change to player
   const displayContent = function () {
     for (let i = 0; i < 9; i++) {
@@ -10,29 +20,45 @@ let gameBoard = (function () {
       div.textContent = grids[i];
     }
   };
-
+  const checkSpot = function (index) {
+    //see if grid is taken
+    return grids[index] == "";
+  };
+  const GameWon = function () {
+    for (const combination of gameGrid) {
+      const [a, b, c] = combination;
+      if (grids[a] != "" && grids[a] === grids[b] && grids[a] === grids[c]) {
+        console.log("at last");
+      }
+    }
+  };
   let gameGrids = function () {
     const spots = document.querySelectorAll(".grid");
     spots.forEach((spot, i) =>
       spot.addEventListener("click", function () {
-        if (firstChoice) {
+        if (firstChoice && checkSpot(i)) {
           grids[i] = "X";
           displayContent();
+          GameWon();
           firstChoice = false;
-        } else {
+        } else if (checkSpot(i)) {
           grids[i] = "O";
           displayContent();
-          console.log(grids);
+          GameWon();
           firstChoice = true;
         }
       })
     );
   };
-
   return {
     displayContent: displayContent,
     clickDivs: gameGrids,
   };
 })();
+const players = function (player) {
+  return {
+    player: player,
+  };
+};
 
 gameBoard.clickDivs();
